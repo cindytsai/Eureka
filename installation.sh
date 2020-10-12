9a92053e116d69feaab1997ba2ae600658c6acfd
cindytsai <6188366+cindytsai@users.noreply.github.com>
2020-10-12 08:00:12 +0800

    Moving this project to Ubuntu. Error within text editor in win10.

--- installation.sh
+++ installation.sh
@@ -1,7 +1,7 @@
 # installation.sh is now at "/".
 echo "      ~ Install Software ~"
 echo "Create directory /software"
-mkdir software
+mkdir /software
 cd software
 echo "Downloading fftw-2.1.5-intel ..."
 curl -o fftw-2.1.5.tar.gz http://www.fftw.org/fftw-2.1.5.tar.gz
