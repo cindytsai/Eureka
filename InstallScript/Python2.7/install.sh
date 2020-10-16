eadb25f861bcc854e958afd23309cf42ec88b1e8
cindytsai <6188366+cindytsai@users.noreply.github.com>
2020-10-17 00:21:51 +0800

    Install Python2.7 and pip successfully.

--- InstallScript/Python2.7/install.sh
+++ InstallScript/Python2.7/install.sh
@@ -1,10 +1,12 @@
-# For later get-pip.py will use.
+# Prerequsite packages for python and pip
 cd /packages
 git clone https://github.com/madler/zlib.git 
 cd zlib
 ./configure --prefix=/usr/local
 make
 make install
+cd /packages
+yum -y install openssl-devel openssl
 # For Python2.7.18
 cd /packages
 curl -o Python-2.7.18.tgz https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
