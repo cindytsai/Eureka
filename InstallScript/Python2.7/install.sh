f9604d18ff639dd694527764d50b9947f8b964f6
cindytsai <6188366+cindytsai@users.noreply.github.com>
2020-10-16 16:02:57 +0800

    Install Python2.7.18

--- InstallScript/Python2.7/install.sh
+++ InstallScript/Python2.7/install.sh
@@ -3,3 +3,6 @@ curl -o Python-2.7.18.tgz https://www.python.org/ftp/python/2.7.18/Python-2.7.18
 tar zxvf Python-2.7.18.tgz
 rm -f Python-2.7.18.tgz
 cd Python-2.7.18
+./configure --prefix=/work1/cindytsai/Software/python/python2.7 --enable-shared --enable-ipv6 LDFLAGS=-Wl,-rpath=/work1/cindytsai/Software/python/python2.7/lib,--disable-new-dtags
+make
+make install
