dd1b40c6b2578ec10e2ab39f22890f5d9cb109dc
cindytsai <6188366+cindytsai@users.noreply.github.com>
2020-10-17 15:48:28 +0800

    Install python packages using pip.

--- InstallScript/Python2.7/installPythonPackages.sh
+++ InstallScript/Python2.7/installPythonPackages.sh
@@ -1,3 +1,7 @@
+# Get pip for python2.7
 cd /packages
 curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py
 /work1/cindytsai/Software/python/python2.7/bin/python get-pip.py
+# Install packages for python using pip
+cd /work1/cindytsai/Software/python/python2.7/bin
+./pip install numpy cython jupyter scipy astropy
