a913b5ecfde14b6f5a17afb4ec1c5567a8463c10
cindytsai <6188366+cindytsai@users.noreply.github.com>
2020-10-18 17:40:24 +0800

    Install Python3.8.6 and packages using pip.

---
+++ InstallScript/Python3.8/install.sh
@@ -0,0 +1,24 @@
+# Prerequsite packages for python and pip
+# For zlib , openssl, and libffi-devel
+cd /packages
+git clone https://github.com/madler/zlib.git 
+cd zlib
+./configure --prefix=/usr/local
+make
+make install
+cd /packages
+yum -y install openssl-devel openssl
+yum -y install libffi-devel
+# For Python3.8.6
+cd /packages
+curl -o Python-3.8.6.tgz https://www.python.org/ftp/python/3.8.6/Python-3.8.6.tgz
+tar zxvf Python-3.8.6.tgz
+rm -f Python-3.8.6.tgz
+cd Python-3.8.6
+./configure --prefix=/work1/cindytsai/Software/python/python3.8 --enable-shared --enable-ipv6 LDFLAGS=-Wl,-rpath=/work1/cindytsai/Software/python/python3.8/lib,--disable-new-dtags
+make
+make install
+# Get pip for python3.8
+cd /packages
+curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py
+/work1/cindytsai/Software/python/python3.8/bin/python3.8 get-pip.py
---
+++ InstallScript/Python3.8/installPythonPackages.sh
@@ -0,0 +1,7 @@
+# Install packages for python using pip
+cd /work1/cindytsai/Software/python/python3.8/bin
+./pip install numpy cython jupyter scipy astropy
+HDF5_DIR=/software/hdf5/1.10.6 ./pip install --no-binary=h5py h5py
+LD_LIBRARY_PATH=/software/openmpi/3.1.5-gnu/lib:$LD_LIBRARY_PATH
+PATH=/software/openmpi/3.1.5-gnu/bin:$PATH
+./pip install mpi4py
