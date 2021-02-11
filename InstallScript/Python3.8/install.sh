89e9a2fbb695cf46ca130780f034d358d5bfd3d2
cindytsai <turquoisea.tsai@gmail.com>
2021-02-11 14:34:41 +0800

    Update Python38/Install.sh

--- InstallScript/Python3.8/install.sh
+++ InstallScript/Python3.8/install.sh
@@ -15,7 +15,7 @@ curl -o Python-3.8.6.tgz https://www.python.org/ftp/python/3.8.6/Python-3.8.6.tg
 tar zxvf Python-3.8.6.tgz
 rm -f Python-3.8.6.tgz
 cd Python-3.8.6
-./configure --prefix=/work1/cindytsai/Software/python/python3.8 --enable-shared --enable-ipv6 LDFLAGS=-Wl,-rpath=/work1/cindytsai/Software/python/python3.8/lib,--disable-new-dtags
+./configure --prefix=/work1/cindytsai/Software/python/python3.8 --enable-shared --enable-ipv6 LDFLAGS=-Wl,-rpath=/work1/cindytsai/Software/python/python3.8/lib,--disable-new-dtags --enable-optimizations
 make
 make install
 # Get pip for python3.8
